8328022ceb1abdb93cc67426f9856cbcffb5cf8d
Jose Reginaldo <prof.josereginaldo@gmail.com>
2024-03-15 09:44:59 -0300

    codigos da aula2 a aula5

---
+++ aulas/aula05/router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/produtos", function(req, res) {
+  res.json([]); // 200 e JSON
+});
+
+router.get("/produtos/:produtoId", function(req, res) {
+  if (req.params.produtoId == 1) {
+    res.json({});  //200 e JSON
+  } else {
+    res.status(404).json({msg: "Produto nao encontrado"});
+  }
+});
+
+router.post("/produtos", function(req, res) {
+  if (req.body && req.body.nome && req.body.preco) {
+    res.status(201).json({});
+  } else {
+    res.status(422)
+    .json({msg: "Nome e preco do produto obrigatorios"});
+  }
+})
+
+module.exports = router;
